d7ed1f45c49c3dc9eb3e11cd102ea2842350a772
hahoetal <yeong-eun@likelion.org>
2020-10-03 16:28:03 +0900

    form.py 수정

--- main/form.py
+++ main/form.py
@@ -5,3 +5,35 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['name', 'title', 'content', 'image', 'category', 'url']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = "제품명"
+        self.fields['title'].label = "제목"
+        self.fields['content'].label= "내용"
+        self.fields['category'].label ="분류"
+
+        self.fields['name'].widget.attrs.update({
+            'class':'post_name',
+            'placeholder': '공동구매할 제품명을 입력하세요.',
+        })
+
+        self.fields['title'].widget.attrs.update({
+            'class':'post_title',
+        })
+
+        self.fields['content'].widget.attrs.update({
+            'class':'post_content',
+        })
+
+        self.fields['category'].widget.attrs.update({
+            'class':'post_category',
+        })
+
+        self.fields['image'].widget.attrs.update({
+            'class':'post_image',
+        })
+        self.fields['url'].widget.attrs.update({
+            'class':'post_url',
+            'placeholder': '제품을 판매하는 사이트 주소를 입력해주세요.',
+        })
