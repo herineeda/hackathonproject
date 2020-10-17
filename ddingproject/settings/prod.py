ef62ad2d69633e1d25fda213e1442faf503cb501
Aqudi <gjdigj145@gmail.com>
2020-10-17 17:06:16 +0900

    feat: 세팅파일 분리

---
+++ ddingproject/settings/prod.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DEBUG = bool(os.getenv("DEBUG", True))
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS += [
+
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# 정적파일의 크기를 줄여서 서빙
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
