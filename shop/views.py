a809b01b0da74343a6416e1b58562df7eb0193a0
Aqudi <gjdigj145@gmail.com>
2020-10-26 03:28:25 +0900

    clean: 자동정렬

--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,7 @@ from .models import *
 from .forms import ReviewForm
 from django.core.exceptions import PermissionDenied
 
+
 def product_in_category(request, category_slug=None):
     current_category = None
     categories = Category.objects.all()
@@ -25,14 +26,15 @@ def product_detail(request, id, product_slug=None):
     product = get_object_or_404(
         Product, id=id, slug=product_slug)  # 제품 있으면 불러와짐
     add_to_cart = AddProductForm(initial={'quantity': 1})
-    
-    review_form = ReviewForm() # 리뷰를 작성할 폼 가져오기
+
+    review_form = ReviewForm()  # 리뷰를 작성할 폼 가져오기
     return render(request, 'shop/detail.html', {
         'shop_active': "is-active",
         'product': product,
         'add_to_cart': add_to_cart,
-        'review_form':review_form
+        'review_form': review_form
     })
+
 
 # # 상품 리뷰 달기
 def review(request, id, category, product_slug=None):
@@ -41,7 +43,7 @@ def review(request, id, category, product_slug=None):
     if form.is_valid():
         temp = form.save(commit=False)
         temp.category = Category.objects.get(name=category)
-        temp.product =  get_object_or_404(Product, id=id, slug=product_slug)
+        temp.product = get_object_or_404(Product, id=id, slug=product_slug)
         temp.writer = request.user
         # temp.rate
         temp.date = timezone.datetime.now()
@@ -54,6 +56,7 @@ def review(request, id, category, product_slug=None):
         product.count = r_count
         product.save()
         return redirect('shop:product_detail', id, product_slug)
+
 
 # 리뷰 삭제하기
 def delete_review(request, id, category, review_id, product_slug=None):
@@ -73,10 +76,10 @@ def delete_review(request, id, category, review_id, product_slug=None):
     else:
         raise PermissionDenied
 
+
 # 상품 검색하기
 def search_product(request):
     categories = Category.objects.all()
-
     keyword = request.GET.get('q')
     result = Product.objects.filter(name__contains=keyword)
-    return render(request, 'shop/list.html', {'categories': categories, 'products':result})
+    return render(request, 'shop/list.html', {'categories': categories, 'products': result})
